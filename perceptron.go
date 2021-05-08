a71caab88367ede255597ccba2b318608d86f75a
IntrntSrfr <aleksander@tosbakken.com>
2021-05-08 20:00:47 +0200

    asd

--- perceptron.go
+++ perceptron.go
@@ -1,7 +1,14 @@
 package jarvis
 
 type Perceptron struct {
-	Inputs  Vector
 	Weights Vector
 	Bias    int
+}
+
+func (p Perceptron) Guess(inp Vector) int {
+	if VecSum(inp) >= 0 {
+		return 1
+	} else {
+		return -1
+	}
 }
