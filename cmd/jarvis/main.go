43b61765fd430b3bf9bc47259c124b493472d6db
IntrntSrfr <aleksander@tosbakken.com>
2021-05-09 00:04:32 +0200

    fix bug with pointer receiver

--- cmd/jarvis/main.go
+++ cmd/jarvis/main.go
@@ -22,24 +22,10 @@ func main() {
 	p := jarvis.NewPerceptron(2, 0.1)
 	fmt.Println(p)
 
-	for i := 0; i < 1000; i++ {
+	for i := 0; i < 1; i++ {
 		for _, point := range points {
 			input := jarvis.Vector{point.X, point.Y}
-
 			p.Train(input, point.Label)
-
-			//fmt.Println()
-
-			//fmt.Println(jarvis.VecDot(v1, p.Weights))
-
-			//guess := p.Guess(input)
-			//fmt.Println("GUESS", guess)
-
-			//miss := point.Label - guess
-			//fmt.Println("ERROR:", miss)
-			//fmt.Println(jarvis.VecAdd(p.Weights, jarvis.VecScale(v1, float64(miss))))
-
-			//p.Weights = jarvis.VecAdd(p.Weights, jarvis.VecScale(input, float64(miss)))
 		}
 	}
 	fmt.Println(p)
--- perceptron.go
+++ perceptron.go
@@ -7,32 +7,18 @@ type Perceptron struct {
 
 func NewPerceptron(size int, lr float64) Perceptron {
 	return Perceptron{
-		//Weights: NewRandomVector(size),
-		Weights: Vector{0, 0},
+		Weights: make(Vector, size),
 		Lr:      lr,
 	}
 }
 
-func (p Perceptron) Train(inp Vector, target int) {
+func (p *Perceptron) Train(inp Vector, target int) {
 	guess := p.Guess(inp)
 	miss := target - guess
-
-	for i := range p.Weights {
-		p.Weights[i] += inp[i] * float64(miss) * p.Lr
-	}
-
-	/*
-		lol := VecScale(inp, float64(miss))
-		lol = VecScale(lol, p.Lr)
-		fmt.Println("DELTA W", lol)
-		lol = VecAdd(p.Weights, lol)
-		fmt.Println("NEW W", lol)
-		p.Weights = VecAdd(p.Weights, VecScale(VecScale(inp, float64(miss)), p.Lr))
-	*/
+	p.Weights = VecAdd(p.Weights, VecScale(inp, float64(miss)*p.Lr))
 }
 
-func (p Perceptron) Guess(inp Vector) int {
-
+func (p *Perceptron) Guess(inp Vector) int {
 	if VecDot(p.Weights, inp) >= 0 {
 		return 1
 	} else {
