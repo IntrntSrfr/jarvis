62ed00a0d2c1027f5bc54e9464c678d4bef15c13
IntrntSrfr <aleksander@tosbakken.com>
2021-05-20 17:02:01 +0200

    change point labels to float64

--- point.go
+++ point.go
@@ -1,11 +1,14 @@
 package jarvis
 
-import "math/rand"
+import (
+	"fmt"
+	"math/rand"
+)
 
 type Point struct {
 	X     float64
 	Y     float64
-	Label int
+	Label float64
 }
 
 func NewPoint() Point {
@@ -13,11 +16,15 @@ func NewPoint() Point {
 		X: rand.Float64() * 10,
 		Y: rand.Float64() * 10,
 	}
-	if 0.431*p.X+3 >= p.Y {
+	if -0.75*p.X+6 >= p.Y {
 		//if p.X >= p.Y {
 		p.Label = 1
 	} else {
 		p.Label = 0
 	}
 	return p
+}
+
+func (p Point) String() string {
+	return fmt.Sprintf("(%v, %v)", p.X, p.Y)
 }
