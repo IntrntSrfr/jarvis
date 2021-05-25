4d5603cef7a2d0d5d379019a664345e023cb68ea
IntrntSrfr <aleksander@tosbakken.com>
2021-05-25 13:50:40 +0200

    added training, it seems to work but im not sure

--- cmd/network/main.go
+++ cmd/network/main.go
@@ -1,15 +1,52 @@
 package main
 
 import (
-	"fmt"
 	"github.com/intrntsrfr/jarvis"
 )
 
 func main() {
-	n := jarvis.NewNetwork(3, 4, 2, 0.1)
-	fmt.Println("NETWORK:", n)
+	n := jarvis.NewNetwork(2, 2, 2, 0.1)
+	//fmt.Println("NETWORK:", n)
 
 	//targets := jarvis.Matrix{{3}, {5}}
-	inp := jarvis.Matrix{{3}, {2}, {4}}
-	fmt.Println(n.Feed(inp))
+
+	inp := jarvis.Matrix{{0.5}, {0.10}}
+	target := jarvis.Matrix{{0}, {1}}
+	n.Train(inp, target)
+	for i := 0; i < 1000; i++ {
+		n.Train(inp, target)
+	}
+
+	/*
+		dataset := []struct{
+			inp jarvis.Matrix
+			target jarvis.Matrix
+		}{
+			{
+				inp:    jarvis.Matrix{{0}, {0}},
+				target: jarvis.Matrix{{0}},
+			},
+			{
+				inp:    jarvis.Matrix{{0}, {1}},
+				target: jarvis.Matrix{{0}},
+			},
+			{
+				inp:    jarvis.Matrix{{1}, {0}},
+				target: jarvis.Matrix{{0}},
+			},
+			{
+				inp:    jarvis.Matrix{{1}, {1}},
+				target: jarvis.Matrix{{1}},
+			},
+		}
+		for i:=0;i<100000;i++{
+			for _, data := range dataset{
+				n.Train(data.inp, data.target)
+			}
+		}
+
+		fmt.Println(n.Guess(dataset[3].inp))
+	*/
+	//fmt.Println(n.Guess(inp))
+
 }
--- network.go
+++ network.go
@@ -1,6 +1,9 @@
 package jarvis
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 type Network struct {
 	Inputs    int
@@ -28,26 +31,83 @@ func (n *Network) String() string {
 	return fmt.Sprintf("NETWORK - inputs: %v, hidden: %v, outputs: %v, lr: %v\nIH Weights: \n%v\nHO Weights: \n%v", n.Inputs, n.Hidden, n.Output, n.LR, n.WeightsIH, n.WeightsHO)
 }
 
-// Feed takes an input and feeds it through the network
-func (n *Network) Feed(inp Matrix) Matrix {
+// Guess takes an input and feeds it through the network
+func (n *Network) Guess(inp Matrix) Matrix {
 	// Hidden result: result from IH x inp
-	hr := MatrixMap(MatrixDot(n.WeightsIH, inp), Sigmoid)
-	fmt.Println("HIDDEN RESULT:\n", hr)
+	hiddenResult := MatrixDot(n.WeightsIH, inp)
+	hiddenActivations := MatrixMap(hiddenResult, Sigmoid)
+	//fmt.Println("HIDDEN RESULT:\n", hr)
 
 	// Output result: result from HO x hr
-	or := MatrixMap(MatrixDot(n.WeightsHO, hr), Sigmoid)
-	fmt.Println("OUTPUT RESULT:\n", or)
+	outputResult := MatrixDot(n.WeightsHO, hiddenActivations)
+	outputActivations := MatrixMap(outputResult, Sigmoid)
+	//fmt.Println("OUTPUT RESULT:\n", or)
 
-	return or
+	return outputActivations
 
-	/*
-		errors := MatrixSub(target, or)
-		fmt.Println("ERRORS:\n", errors)
-
-		fmt.Println(MatrixTranspose(n.WeightsHO))
-	*/
 }
 
-func (n *Network) Train() {
+func (n *Network) Train(inp, target Matrix) {
+	// this can probably be changed so that we train a whole dataset, but for now only 1 input
+	// or we can use this method in another method to deal with a whole dataset, in that case we just need to return
+	// the mseErrors this input gives so we can inform about the average error of a dataset
 
+	hiddenResult := MatrixDot(n.WeightsIH, inp)
+	hiddenActivations := MatrixMap(hiddenResult, Sigmoid)
+	//fmt.Println("HIDDEN RESULT:\n", hr)
+
+	// Output result: result from HO x hr
+	outputResult := MatrixDot(n.WeightsHO, hiddenActivations)
+	outputActivations := MatrixMap(outputResult, Sigmoid)
+
+	mseErrors := make(Matrix, n.Output)
+
+	totalError := 0.0
+	for i := range outputActivations {
+		outputError := 0.5 * math.Pow(target[i][0]-outputActivations[i][0], 2)
+		totalError += outputError
+		mseErrors[i] = Vector{outputError}
+		//fmt.Println(fmt.Sprintf("error for output %v: %v", i, outputError))
+	}
+	fmt.Println("total error", totalError)
+	//fmt.Println(fmt.Sprintf("HIDDEN OUTPUTS: \n\t%v", hiddenActivations))
+	//fmt.Println(fmt.Sprintf("FINAL OUTPUTS: \n\t%v", outputActivations))
+	//fmt.Println(fmt.Sprintf("FINAL MSE ERRORS: \n\t%v", mseErrors))
+
+	errors := MatrixSub(target, outputActivations)
+	errors = MatrixScale(errors, -1)
+	//fmt.Println(errors)
+
+	finalSubtracted := subtractMatrix(outputActivations)
+	hiddenSubtracted := subtractMatrix(hiddenActivations)
+
+	//fmt.Println(fmt.Sprintf("1 - FINAL OUTPUTS: \n\t%v", finalSubtracted))
+
+	hiddenErrors := MatrixDot(MatrixTranspose(n.WeightsHO), errors)
+
+	updatedHO := MatrixScale(MatrixDot(MatrixMultiply(errors, MatrixMultiply(outputActivations, finalSubtracted)), MatrixTranspose(hiddenActivations)), n.LR)
+	updatedIH := MatrixScale(MatrixDot(MatrixMultiply(hiddenErrors, MatrixMultiply(hiddenActivations, hiddenSubtracted)), MatrixTranspose(inp)), n.LR)
+
+	//fmt.Println(fmt.Sprintf("IH WEIGHTS:\n\t%v", n.WeightsIH))
+	//fmt.Println(fmt.Sprintf("HO WEIGHTS:\n\t%v", n.WeightsHO))
+
+	//fmt.Println(fmt.Sprintf("CHANGE IH WEIGHTS:\n\t%v", updatedIH))
+	//fmt.Println(fmt.Sprintf("CHANGE HO WEIGHTS:\n\t%v", updatedHO))
+
+	n.WeightsIH = MatrixSub(n.WeightsIH, updatedIH)
+	n.WeightsHO = MatrixSub(n.WeightsHO, updatedHO)
+}
+
+func subtractMatrix(m Matrix) Matrix {
+	res := Matrix{}
+
+	for i := range m {
+		var v []float64
+		for j := range m[0] {
+			v = append(v, 1-m[i][j])
+		}
+		res = append(res, v)
+	}
+
+	return res
 }
