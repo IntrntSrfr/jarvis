9f3bdb2196c7f8e0945cf370979e6192bc1e1a19
IntrntSrfr <aleksander@tosbakken.com>
2021-05-26 03:36:53 +0200

    xor more or less complete?

---
+++ cmd/xor/main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"fmt"
+	"github.com/intrntsrfr/jarvis"
+	"math/rand"
+	"time"
+)
+
+func init() {
+	rand.Seed(time.Now().Unix())
+}
+
+func main() {
+	dataset := []data{
+		{
+			labels: jarvis.Matrix{{1}, {0}},
+			data:   jarvis.Matrix{{0}, {0}},
+		},
+		{
+			labels: jarvis.Matrix{{0}, {1}},
+			data:   jarvis.Matrix{{0}, {1}},
+		},
+		{
+			labels: jarvis.Matrix{{0}, {1}},
+			data:   jarvis.Matrix{{1}, {0}},
+		},
+		{
+			labels: jarvis.Matrix{{1}, {0}},
+			data:   jarvis.Matrix{{1}, {1}},
+		},
+	}
+
+	fmt.Println(len(dataset))
+
+	n := jarvis.NewNetwork(2, 4, 2, .1)
+
+	for i := 0; i < 10000; i++ {
+		t := 0.0
+		for _, d := range dataset {
+			t += n.Train(d.data, d.labels)
+		}
+		if i%10 == 0 {
+			fmt.Println(fmt.Sprintf("> EPOCH %v - TOTAL ERROR: %v - TOTAL AVERAGE ERROR: %v", i, t, t/float64(len(dataset))))
+		}
+	}
+
+	fmt.Println(fmt.Sprintf("(0, 0) GUESS: %v; WANT [1, 0]", n.Guess(jarvis.Matrix{{0}, {0}})))
+	fmt.Println(fmt.Sprintf("(0, 1) GUESS: %v; WANT [0, 1]", n.Guess(jarvis.Matrix{{0}, {1}})))
+	fmt.Println(fmt.Sprintf("(1, 0) GUESS: %v; WANT [0, 1]", n.Guess(jarvis.Matrix{{1}, {0}})))
+	fmt.Println(fmt.Sprintf("(1, 1) GUESS: %v; WANT [1, 0]", n.Guess(jarvis.Matrix{{1}, {1}})))
+}
+
+type data struct {
+	labels jarvis.Matrix
+	data   jarvis.Matrix
+}
