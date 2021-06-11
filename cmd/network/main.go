c3d83406df434905019930ab5ff49c2d76b35542
internetsurfer <aleksander@tosbakken.com>
2021-06-11 23:30:04 +0200

    main fc

--- cmd/network/main.go
+++ cmd/network/main.go
@@ -1,52 +1,25 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/intrntsrfr/jarvis"
 )
 
 func main() {
-	n := jarvis.NewNetwork(2, 2, 2, 0.1)
-	//fmt.Println("NETWORK:", n)
 
-	//targets := jarvis.Matrix{{3}, {5}}
+	n := jarvis.NewNetwork2()
 
-	inp := jarvis.Matrix{{0.5}, {0.10}, {1}}
-	target := jarvis.Matrix{{0}, {1}}
-	n.Train(inp, target)
-	for i := 0; i < 1000; i++ {
-		n.Train(inp, target)
+	n.AddLayer(2)
+	n.AddLayer(4)
+	n.AddLayer(4)
+	n.AddLayer(1)
+
+	n.Compile()
+
+	for i := 0; i < 500; i++ {
+		lol := n.Train2([]float64{0, 1}, []float64{1}, 0.1)
+		fmt.Println(lol)
 	}
-
-	/*
-		dataset := []struct{
-			inp jarvis.Matrix
-			target jarvis.Matrix
-		}{
-			{
-				inp:    jarvis.Matrix{{0}, {0}},
-				target: jarvis.Matrix{{0}},
-			},
-			{
-				inp:    jarvis.Matrix{{0}, {1}},
-				target: jarvis.Matrix{{0}},
-			},
-			{
-				inp:    jarvis.Matrix{{1}, {0}},
-				target: jarvis.Matrix{{0}},
-			},
-			{
-				inp:    jarvis.Matrix{{1}, {1}},
-				target: jarvis.Matrix{{1}},
-			},
-		}
-		for i:=0;i<100000;i++{
-			for _, data := range dataset{
-				n.Train(data.inp, data.target)
-			}
-		}
-
-		fmt.Println(n.Guess(dataset[3].inp))
-	*/
-	//fmt.Println(n.Guess(inp))
 
 }
